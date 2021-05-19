5ee573482e2331e617531fb904b4d6d7ec5f05eb
Evan Chang <evanc577@gmail.com>
2021-05-19 12:42:54 -0400

    Use stdlib sort

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,24 @@
-use rand::seq::SliceRandom;
-use rand::thread_rng;
-use rand::Rng;
+mod element;
+
+use self::element::Element;
+use itertools::Itertools;
 use std::{
-    fmt,
     fs::File,
-    io::{prelude::*, stdin, stdout, BufReader},
+    io::{prelude::*, BufReader},
     path::Path,
 };
-
-enum Choice {
-    Left,
-    Right,
-}
 
 fn main() {
     // read input data
     let candidates = lines_from_file("input.txt");
-    println!("{:?}", candidates);
-    let candidate_refs: Vec<&str> = candidates.iter().map(|s| s.as_str()).collect();
 
     // sort data
-    let order = kwiksort(candidate_refs);
+    let order = candidates.iter().map(|s| Element::new(s.as_str())).sorted();
+
+    // display ordering
     println!();
     println!("Final ordering:");
-    for (i, s) in order.iter().enumerate() {
+    for (i, s) in order.enumerate() {
         println!("{}. {}", i + 1, s);
     }
 }
@@ -34,58 +29,4 @@ fn lines_from_file(filename: impl AsRef<Path>) -> Vec<String> {
     buf.lines()
         .map(|l| l.expect("Could not parse line"))
         .collect()
-}
-
-fn kwiksort<T: fmt::Display + ?Sized>(list: Vec<&T>) -> Vec<&T> {
-    if list.len() <= 1 {
-        return list;
-    }
-
-    let mut rng = thread_rng();
-
-    // Choose random pivot
-    let i: usize = rng.gen_range(0..list.len());
-    let mut list_l = vec![];
-    let mut list_r = vec![];
-
-    // iterate through all elements except pivot
-    let mut iter = list[0..i]
-        .iter()
-        .chain(list[i + 1..list.len()].iter())
-        .collect::<Vec<_>>();
-
-    // pairwise comparison of pivot with every other element
-    iter.shuffle(&mut rng);
-    for e in iter {
-        match compare(list[i], e) {
-            Choice::Left => list_l.push(*e),
-            Choice::Right => list_r.push(*e),
-        }
-    }
-
-    // concatenate
-    let mut ret = kwiksort(list_l);
-    ret.push(list[i]);
-    ret.extend(kwiksort(list_r));
-    ret
-}
-
-fn compare<T: fmt::Display + ?Sized>(choice_1: &T, choice_2: &T) -> Choice {
-    println!();
-    println!("Pick:");
-    println!("(1) {}", choice_1);
-    println!("(2) {}", choice_2);
-    print!("Your choice: ");
-    stdout().flush().unwrap();
-
-    let mut buffer = String::new();
-    stdin().lock().read_line(&mut buffer).unwrap();
-    let buffer = buffer.trim();
-    if buffer == "1" {
-        return Choice::Right;
-    } else if buffer == "2" {
-        return Choice::Left;
-    }
-    println!("{:?}", buffer);
-    panic!("invalid input!");
 }
